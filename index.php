97340ce9b79a8db2f4d6219ba11610815bab901e
Ho33ein Beiki <ho33ein.b@gmail.com>
2016-06-11 15:51:35 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>How to Make Delete Multiple Rows Using PHP</title>
+<link rel="stylesheet" href="style.css"/>
+<script type="text/javascript" src="jquery.min.js"></script>
+<script type="text/javascript">
+function delete_confirm(){
+	var result = confirm("Are you sure to delete users?");
+	if(result){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+$(document).ready(function(){
+    $('#select_all').on('click',function(){
+        if(this.checked){
+            $('.checkbox').each(function(){
+                this.checked = true;
+            });
+        }else{
+             $('.checkbox').each(function(){
+                this.checked = false;
+            });
+        }
+    });
+	
+	$('.checkbox').on('click',function(){
+		if($('.checkbox:checked').length == $('.checkbox').length){
+			$('#select_all').prop('checked',true);
+		}else{
+			$('#select_all').prop('checked',false);
+		}
+	});
+});
+</script>
+</head>
+
+<body>
+<?php session_start();
+if(!empty($_SESSION['success_msg'])) {
+    ?>
+
+<div class="alert alert-success"><?php echo $_SESSION['success_msg'];
+    ?>
+</div>
+<?php unset($_SESSION['success_msg']); } ?>
+<?php
+include_once('dbConfig.php');
+$query = mysqli_query($conn,"SELECT * FROM users");
+?>
+<form name="bulk_action_form" action="action.php" method="post" onSubmit="return delete_confirm();"/>
+    <table class="bordered">
+        <thead>
+        <tr>
+            <th><input type="checkbox" name="select_all" id="select_all" value=""/></th>        
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+        </tr>
+        </thead>
+        <?php
+            if(mysqli_num_rows($query) > 0){
+                while($row = mysqli_fetch_assoc($query)){
+        ?>
+        <tr>
+            <td align="center"><input type="checkbox" name="checked_id[]" class="checkbox" value="<?php echo $row['id']; ?>"/></td>        
+            <td><?php echo $row['first_name']; ?></td>
+            <td><?php echo $row['last_name']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['phone']; ?></td>
+        </tr> 
+        <?php } }else{ ?>
+            <tr><td colspan="5">No records found.</td></tr> 
+        <?php } ?>
+    </table>
+    <input type="submit" class="btn btn-danger" name="bulk_delete_submit" value="Delete"/>
+</form>
+</body>
+
+</html> 
---
+++ users.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2016 at 01:19 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `phone`) VALUES
+(1, 'Hossein', 'Beiki', 'ho33ein.b@gmail.com', '123456'),
+(2, 'Saeed', 'ArianManesh', 'test@gamil.com', '123456'),
+(3, 'Ehsan', 'Seyedi', 'test@gamil.com', '123456'),
+(4, 'Sajad', 'HajiRezaei', 'test@gamil.com', '123456'),
+(5, 'Mostafa', 'Asadi', 'test@gamil.com', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
